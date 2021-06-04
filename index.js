75158a4d12d8c1868a18a79558de2206a8be9d05
Jesse Wenger <wengerj@gmail.com>
2021-06-04 19:48:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const driver = [];
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = { ...driver };
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  console.log(driver);
+}
+
+function deleteFromDriverByKey() {
+
+}
+
+function destructivelyDeleteFromDriverByKey() {
+
+}
